be7a2af9f1c79a77baa0a6e1c390810fe31ad0bd
jpenha1219 <40612781+jpenha1219@users.noreply.github.com>
2021-11-09 19:19:44 -0500

    Update Question3.java

--- Laboratorio5/Complejidad/Question3.java
+++ Laboratorio5/Complejidad/Question3.java
@@ -6,7 +6,7 @@
 
 import java.util.Scanner;
 
-public class E003_Q3_TimeComplexity {
+public class Question3 {
 
 	public static void main(String[] args) {
 		int n;
@@ -17,11 +17,9 @@ public class E003_Q3_TimeComplexity {
 	}
 	
 	public static void parNumberList(int n) {
-
-		for (int i = 0; i < n; i = i+2) {//		
-			//statement				|		|= O(n/2)
-			System.out.println(i);	 //	= O(1)	
-		}   //						|		
+		for (int i = 0; i < n; i = i+2) {
+			System.out.println(i);	
+		}  	
 	}
   //|O(n/2)		|= O(1) * 0(n/2)
   //|		      |= O(n)
