239b20ac7f26b0dd439a8afe85016f2a4ac40a3a
jpenha1219 <40612781+jpenha1219@users.noreply.github.com>
2021-11-23 00:41:57 -0500

    Create MergeKSorted.py

---
+++ Laboratorio7/MergeKSorted.py
@@ -0,0 +1,18 @@
+# MergeKSorted
+# Author: Peña Ugarte David Jose
+# Lista final de prioridad
+# CUI: 20130875
+
+entrada =[[1,4,5],[1,3,4],[2,6]]
+salida=[]
+priorityQueue = queue.PriorityQueue()
+for i in entrada:
+    for j in i: 
+        print('# PriorityQ process:',priorityQueue.queue)
+        priorityQueue.put(j)
+
+print('Salida desordenada:',priorityQueue.queue)
+while not priorityQueue.empty():
+    salida.append(priorityQueue.get()) 
+#print("# result: ---------------------------")
+print('Salida ordenada:',salida)
