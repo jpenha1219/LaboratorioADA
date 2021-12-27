d4e72cfde0d7240d69c912ab2fdbe86b1d010edc
jpenha1219 <40612781+jpenha1219@users.noreply.github.com>
2021-12-27 02:12:53 -0500

    Create MaxNonoverlappingSegments.java

---
+++ Laboratorio9/MaxNonoverlappingSegments.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class MaxNonoverlappingSegments {
+        public static void main(String args[]){
+                int[] A = { 1, 3, 7, 9, 9};
+		int[] B = { 5, 6, 8, 9, 10};
+                System.out.print(solution(A, B));
+
+        }
+        
+        public static int solution(int[] A, int[] B) {
+                int N = A.length;
+                if (N <= 1) {
+                    return N;
+                }
+                int cnt = 1;
+                int prev_end = B[0];
+                int curr;
+                for (curr = 1; curr < N; curr++) {
+                    if (A[curr] > prev_end) {
+                        cnt++;
+                        prev_end = B[curr];
+                    }
+                }
+                return cnt;
+        }
+}
