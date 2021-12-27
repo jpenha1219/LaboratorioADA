eeec8ed8db6b42b98a9ef25412c2fa41f23789db
jpenha1219 <40612781+jpenha1219@users.noreply.github.com>
2021-12-27 00:43:27 -0500

    Update UniquePathsII.java

--- Laboratorio8/UniquePathsII.java
+++ Laboratorio8/UniquePathsII.java
@@ -2,10 +2,10 @@
 public class UniquePathsII {
     public static void main(String[] args){
       int [][] obstacleGrid = {{0,0,0},{0,1,0},{0,0,0}};
-      uniquePathsWithObstacles(obstacleGrid);
+      System.out.print(uniquePathsWithObstacles(obstacleGrid));
     }
 
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
         if (obstacleGrid[0][0] == 1) return 0;
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
